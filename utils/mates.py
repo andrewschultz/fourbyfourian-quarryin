f9d1eea7d71de6ac3c56ef5f89d03408e9c4110c
Andrew <blurglecruncheon@gmail.com>
2021-07-15 11:39:25 -0500

    eliminated identical solutions (N1/N2 or B1/B2 flipped), improved output

--- utils/mates.py
+++ utils/mates.py
@@ -80,13 +80,16 @@ def print_board(my_perm, blocked, knight_1, knight_2):
 # 3 = my king
 
 def get_mates(knight_1, knight_2, wanted_mate = BOTH_MATES):
-    print("=========================BEGINNING", mate_type[wanted_mate], "checking for {} and {}.".format(pieces[knight_1], pieces[knight_2]))
+    this_mate = "{} / {} {}".format(pieces[knight_1], pieces[knight_2], mate_type[wanted_mate])
+    print("=========================BEGINNING check for {}.".format(this_mate))
     count = 0 # 0 = enemy king, 1 = 1st piece, 2 = 2nd piece, 3 = your king
     for p in lp:
         if p[0][0] in (0, 1, 4): continue
         if p[0][1] > 0: continue
         if p[1][1] > 2: continue
         if p[2][1] > 2: continue
+        if knight_1 == knight_2 and p[1] > p[2]:
+            continue
         if p[3][1] > 2: continue
         if abs(p[3][1] - p[0][1]) <= 1 and abs(p[3][0] - p[0][0]) <= 1: continue
         blocked = defaultdict(bool)
@@ -119,12 +122,16 @@ def get_mates(knight_1, knight_2, wanted_mate = BOTH_MATES):
         if not trapped:
             continue
         count += 1
-        print("BAM! Solution # {} for {} {} {}.".format(count, pieces[knight_1], pieces[knight_2], mate_type[wanted_mate]))
+        print("BAM! Solution # {} for {}.".format(count, this_mate))
         print_board(p, blocked, knight_1, knight_2)
-    print("Total # of mates =", count)
+    return "Total # for {} = {}\n".format(this_mate, count)
+
+end_string = ''
 
 for x in (STALEMATE, CHECKMATE):
-    get_mates(True, True, x)
-    get_mates(True, False, x)
-    get_mates(False, True, x)
-    get_mates(False, False, x)
+    end_string += get_mates(True, True, x)
+    end_string += get_mates(True, False, x)
+    end_string += get_mates(False, True, x)
+    end_string += get_mates(False, False, x)
+
+print(end_string)
